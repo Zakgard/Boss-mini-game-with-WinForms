71ab56705ac3fec2cdb8be181e1dd9449eb68e10
Zakgard <mr.zakhar21@gmail.com>
2022-09-27 14:25:05 +0300

    completed

---
+++ Бой с боссом/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Бой_с_боссом
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(482, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Атака";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(588, 229);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Выход";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Вызов голлема (Наносит боссу 800 урона, потребляет 100 МП)",
+            "Пространственный разлом (Восстанавливает игроку 100 ХП, тратит 50 МП)",
+            "Проклятие смерти (Наносит 600 урона боссу, тратит 100 ХП)",
+            "Исцеление (восстанаяливает 400 ХП, тратит 200 МП)"});
+            this.comboBox1.Location = new System.Drawing.Point(69, 203);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(407, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(373, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Босс";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Игрок";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(69, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(69, 118);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(376, 51);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(69, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "ХП";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(376, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "ХП";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(69, 145);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Мана";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(698, 292);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Бой с боссом/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Бой_с_боссом
+{
+    public partial class Form1 : Form
+    {
+        private double _totalPlayersHp=500;
+        private double _totalPlayersMp=200;       
+        private double _totalBossHP=5000;
+        private ToolTip _toolTip = new ToolTip();
+        private string _attackTypePlayer;
+
+        private int _attacTypePlayserNumber;
+        private int _bossAttackTypeNumber;
+
+        private bool _isAttackComplete=false;
+        
+        Random random=new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ChoosePlayerTypeAttack()
+        {
+            _attackTypePlayer = comboBox1.Text;
+            switch (_attackTypePlayer)
+            {
+                case "Вызов голлема (Наносит боссу 800 урона, потребляет 100 МП)":
+                    _attacTypePlayserNumber = 1;
+                    break;
+                case "Пространственный разлом (Восстанавливает игроку 100 ХП, тратит 50 МП)":
+                    _attacTypePlayserNumber = 2;
+                    break;
+                case "Проклятие смерти (Наносит 600 урона боссу, тратит 100 ХП)":
+                    _attacTypePlayserNumber = 3;
+                    break;
+                case "Исцеление (восстанаяливает 400 ХП, тратит 200 МП)":
+                    _attacTypePlayserNumber = 4;
+                    break;
+                default:
+                    MessageBox.Show("Вы использовали несуществующую атаку!");
+                    break;
+
+            }
+            PlayerAttack(_attacTypePlayserNumber);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ChoosePlayerTypeAttack();
+            CheckIfBossDead();
+            CheckIfPlayerDead();
+        }
+        
+        private void PlayerAttack(int attackType)
+        {
+            while (!_isAttackComplete)
+            {
+                if(attackType == 1)
+                   {
+                if (_totalPlayersMp >= 100)
+                {
+                _totalPlayersMp -= 100;
+                _totalBossHP -= 800;
+                    MessageBox.Show("Вы атаковали босса, используя вызов голлема! Вы нанесли ему 800 урона!");
+                        _isAttackComplete = true;
+                }
+                else
+                {
+                    MessageBox.Show("Нехватка маны!");
+                }
+              
+            }
+            if(attackType == 2)
+                {
+                if(_totalPlayersMp <= 50)
+                {
+                   _totalPlayersMp -= 50;
+                   _totalPlayersHp += 100;
+                    MessageBox.Show("Вы использовали пространственный разлом! Вы восстановили 100 ХП!");
+                    _isAttackComplete = true;
+                    }
+                else
+                {
+                    MessageBox.Show("Нехватка маны!");
+                }
+               
+            }
+            if (attackType == 3)
+            {
+                if (_totalPlayersHp > 100)
+                {
+                    _totalPlayersHp -= 100;
+                    _totalBossHP -= 600;
+                    MessageBox.Show("Вы атаковали босса, используя проклятие смерти! Вы нанесли ему 600 урона!");
+                        _isAttackComplete = true;
+                    }
+                else
+                {
+                    MessageBox.Show("Нехватка здоровья!");
+                }
+                
+            }
+            if (attackType == 4)
+            {
+                if (_totalPlayersMp >= 200)
+                {
+                _totalPlayersMp -= 200;
+                _totalPlayersHp += 400;
+                MessageBox.Show("Вы использовали исцеление! Вы восстановили 400 ХП!");
+                        _isAttackComplete = true;
+                    }
+                else
+                {
+                    MessageBox.Show("Нехватка маны!");
+                }
+               
+            }
+            }
+
+            _isAttackComplete = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = Convert.ToString(_totalPlayersHp);
+            textBox2.Text = Convert.ToString(_totalPlayersMp);
+            textBox3.Text = Convert.ToString(_totalBossHP);
+            
+        }
+             
+
+        private void BossAttack()
+        {
+            _bossAttackTypeNumber=random.Next(1, 2);
+            switch (_bossAttackTypeNumber)
+            {
+                case 1:
+                    _totalPlayersHp -= 100;
+                    MessageBox.Show("Босс вас атаковал используя обычную атаку! Вы потеряли 100 ХП!");
+                    break;
+                case 2:
+                    _totalPlayersHp -= 300;
+                    MessageBox.Show("Босс вас атаковал используя сокрушительную атаку! Вы потеряли 300 ХП!");
+                    break;
+            }
+            textBox3.Text = Convert.ToString(_totalBossHP);
+        }
+
+        private void CheckIfBossDead()
+        {
+            if (_totalBossHP <= 0)
+            {
+                MessageBox.Show("Вы одолелм босса!");
+                textBox3.Text = Convert.ToString(0);
+            }else
+                BossAttack(); ;
+           
+        }
+
+        private void CheckIfPlayerDead()
+        {
+            if (_totalPlayersHp <= 0)
+            {
+                textBox1.Text = Convert.ToString(0);
+                textBox2.Text = Convert.ToString(_totalPlayersMp);
+                MessageBox.Show("Вы проиграли!");
+            }
+            else
+            {
+                textBox1.Text = Convert.ToString(_totalPlayersHp);
+                textBox2.Text = Convert.ToString(_totalPlayersMp);
+                MessageBox.Show("Конец раунда! Вы восстанавливаете 100 МП и 50 ХП");
+                _totalPlayersMp += 100;
+                _totalPlayersHp += 50;
+            }
+        }
+    }
+}
